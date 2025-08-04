ecccb65ece27397c3e6fa672c6914d7758d7829d
Archit Lalwani <architlalwani2002@gmail.com>
2025-08-04 15:00:59 +0530

    Show Login as home for non-logged-in users, Home if logged in

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Navbar from './components/Navbar';
+import UserProfile from './pages/UserProfile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/create" element={<CreatePost />} />
+        <Route path="/user/:id" element={<UserProfile />} />
       </Routes>
     </Router>
   );
